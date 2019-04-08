0a3ffcc93ce6a69adf490c10f39669d1b6c89138
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-07 21:34:42 -0700

    changed minesweeper view.

--- src/minesweeper/Controller/MinesweeperController.java
+++ src/minesweeper/Controller/MinesweeperController.java
@@ -1,4 +1,11 @@
 package minesweeper.Controller;
 
 public class MinesweeperController {
+    public static void main(String[] args) {
+
+        MinesweeperBoard minesweeperBoard = new MinesweeperBoard(10, 10);
+
+
+    }
+
 }
