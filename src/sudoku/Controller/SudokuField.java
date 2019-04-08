dce4bea5753715235d449aa4aa0cab1203522009
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-08 15:26:54 -0700

    for to foreach boolean to void

--- src/sudoku/Controller/SudokuField.java
+++ src/sudoku/Controller/SudokuField.java
@@ -18,11 +18,7 @@ class SudokuField {
         setRandomTemplate();
     }
 
-    boolean setNumber(int x, int y, int value) {
-
-        if(value < 0 || 9 < value) {
-            return false;
-        }
+    void setNumber(int x, int y, int value) {
 
         for (Coordinate square : this.sudokuField) {
 
@@ -31,7 +27,6 @@ class SudokuField {
                 square.setValue(value);
             }
         }
-        return true;
     }
 
     private void setRandomTemplate(){
@@ -116,116 +111,119 @@ class SudokuField {
         int box8 = 0;
         int box9 = 0;
 
-        for(int i = 0; i < this.sudokuField.size(); i++) {
-
-            if(this.sudokuField.get(i).getxCoordinate() == 1) {
-                x1 += this.sudokuField.get(i).getValue();
+        for (Coordinate coordinate : this.sudokuField) {
+            
+            if(coordinate.getxCoordinate() == 1) {
+                x1 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getxCoordinate() == 2) {
-                x2 += this.sudokuField.get(i).getValue();
+            if(coordinate.getxCoordinate() == 1) {
+                x1 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getxCoordinate() == 3) {
-                x3 += this.sudokuField.get(i).getValue();
+            if(coordinate.getxCoordinate() == 2) {
+                x2 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getxCoordinate() == 4) {
-                x4 += this.sudokuField.get(i).getValue();
+            if(coordinate.getxCoordinate() == 3) {
+                x3 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getxCoordinate() == 5) {
-                x5 += this.sudokuField.get(i).getValue();
+            if(coordinate.getxCoordinate() == 4) {
+                x4 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getxCoordinate() == 6) {
-                x6 += this.sudokuField.get(i).getValue();
+            if(coordinate.getxCoordinate() == 5) {
+                x5 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getxCoordinate() == 7) {
-                x7 += this.sudokuField.get(i).getValue();
+            if(coordinate.getxCoordinate() == 6) {
+                x6 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getxCoordinate() == 8) {
-                x8 += this.sudokuField.get(i).getValue();
+            if(coordinate.getxCoordinate() == 7) {
+                x7 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getxCoordinate() == 9) {
-                x9 += this.sudokuField.get(i).getValue();
+            if(coordinate.getxCoordinate() == 8) {
+                x8 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getyCoordinate() == 1) {
-                y1 += this.sudokuField.get(i).getValue();
+            if(coordinate.getxCoordinate() == 9) {
+                x9 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getyCoordinate() == 2) {
-                y2 += this.sudokuField.get(i).getValue();
+            if(coordinate.getyCoordinate() == 1) {
+                y1 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getyCoordinate() == 3) {
-                y3 += this.sudokuField.get(i).getValue();
+            if(coordinate.getyCoordinate() == 2) {
+                y2 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getyCoordinate() == 4) {
-                y4 += this.sudokuField.get(i).getValue();
+            if(coordinate.getyCoordinate() == 3) {
+                y3 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getyCoordinate() == 5) {
-                y5 += this.sudokuField.get(i).getValue();
+            if(coordinate.getyCoordinate() == 4) {
+                y4 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getyCoordinate() == 6) {
-                y6 += this.sudokuField.get(i).getValue();
+            if(coordinate.getyCoordinate() == 5) {
+                y5 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getyCoordinate() == 7) {
-                y7 += this.sudokuField.get(i).getValue();
+            if(coordinate.getyCoordinate() == 6) {
+                y6 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getyCoordinate() == 8) {
-                y8 += this.sudokuField.get(i).getValue();
+            if(coordinate.getyCoordinate() == 7) {
+                y7 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getyCoordinate() == 9) {
-                y9 += this.sudokuField.get(i).getValue();
+            if(coordinate.getyCoordinate() == 8) {
+                y8 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getBoxCoordinate() == 1) {
-                box1 += this.sudokuField.get(i).getValue();
+            if(coordinate.getyCoordinate() == 9) {
+                y9 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getBoxCoordinate() == 2) {
-                box2 += this.sudokuField.get(i).getValue();
+            if(coordinate.getBoxCoordinate() == 1) {
+                box1 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getBoxCoordinate() == 3) {
-                box3 += this.sudokuField.get(i).getValue();
+            if(coordinate.getBoxCoordinate() == 2) {
+                box2 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getBoxCoordinate() == 4) {
-                box4 += this.sudokuField.get(i).getValue();
+            if(coordinate.getBoxCoordinate() == 3) {
+                box3 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getBoxCoordinate() == 5) {
-                box5 += this.sudokuField.get(i).getValue();
+            if(coordinate.getBoxCoordinate() == 4) {
+                box4 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getBoxCoordinate() == 6) {
-                box6 += this.sudokuField.get(i).getValue();
+            if(coordinate.getBoxCoordinate() == 5) {
+                box5 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getBoxCoordinate() == 7) {
-                box7 += this.sudokuField.get(i).getValue();
+            if(coordinate.getBoxCoordinate() == 6) {
+                box6 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getBoxCoordinate() == 8) {
-                box8 += this.sudokuField.get(i).getValue();
+            if(coordinate.getBoxCoordinate() == 7) {
+                box7 += coordinate.getValue();
             }
 
-            if(this.sudokuField.get(i).getBoxCoordinate() == 9) {
-                box9 += this.sudokuField.get(i).getValue();
+            if(coordinate.getBoxCoordinate() == 8) {
+                box8 += coordinate.getValue();
             }
 
+            if(coordinate.getBoxCoordinate() == 9) {
+                box9 += coordinate.getValue();
+            }
         }
 
         if(x1 != 45) {
