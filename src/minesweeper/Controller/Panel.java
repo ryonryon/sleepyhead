a941386393243537fe86d61a0d0c837842fb7b62
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-07 19:51:45 -0700

    change Panel.java

--- src/minesweeper/Controller/Panel.java
+++ src/minesweeper/Controller/Panel.java
@@ -4,11 +4,12 @@ public class Panel {
 
     private int xCoordinate;
     private int yCoordinate;
-    private State state;
+    private BoxStatus boxStatus;
     private BoxValue boxValue;
     private int bomNumberAround;
 
     public Panel(int xCoordinate, int yCoordinate, BoxValue boxValue, int bomNumberAround) {
+
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
         this.boxValue = boxValue;
@@ -17,38 +18,58 @@ public class Panel {
 
     public StateAfterBoxOpen boxOpen() {
 
-        if(this.state == State.flagged || this.state == State.Opened) {
+        if(this.boxStatus == BoxStatus.flagged || this.boxStatus == BoxStatus.Opened) {
             return StateAfterBoxOpen.Nothing;
         }
 
-        if(this.state == State.Close && this.boxValue == BoxValue.Bom) {
+        if(this.boxStatus == BoxStatus.Close && this.boxValue == BoxValue.Bom) {
             return StateAfterBoxOpen.GameOver;
         }
 
-        if(this.state == State.Close && this.boxValue == BoxValue.Blank) {
-            return StateAfterBoxOpen.Nothing;
+        if(this.boxStatus == BoxStatus.Close && this.boxValue == BoxValue.Blank) {
+            return StateAfterBoxOpen.Inducible;
         }
 
-        if(this.state == State.Close && this.boxValue == BoxValue.Numbers) {
-            return StateAfterBoxOpen.Inducible;
+        if(
+            this.boxStatus == BoxStatus.Close
+            && this.boxValue == BoxValue.Number_1
+            || this.boxValue == BoxValue.Number_2
+            || this.boxValue == BoxValue.Number_3
+            || this.boxValue == BoxValue.Number_4
+            || this.boxValue == BoxValue.Number_5
+            || this.boxValue == BoxValue.Number_6
+            || this.boxValue == BoxValue.Number_7
+            || this.boxValue == BoxValue.Number_8
+        ) {
+            return StateAfterBoxOpen.Nothing;
         }
 
         return StateAfterBoxOpen.Error;
     }
 
-    private enum State{
+    private enum BoxStatus{
+
         Opened,
         Close,
         flagged
     }
 
     public enum BoxValue{
+
         Bom,
         Blank,
-        Numbers
+        Number_1,
+        Number_2,
+        Number_3,
+        Number_4,
+        Number_5,
+        Number_6,
+        Number_7,
+        Number_8
     }
 
     public enum StateAfterBoxOpen{
+        
         Nothing,
         GameOver,
         Inducible,
